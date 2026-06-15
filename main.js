95f93a8d7b1c287500bed2df32851929a9936967
assalikiin <fairuznajib04@gmail.com>
2026-06-15 19:51:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -243,3 +243,37 @@ isLight ? "light" : "dark"
 );
 
 });
+
+const targetDate =
+new Date("September 13, 2027 00:00:00").getTime();
+
+function updateCountdown(){
+
+const now = new Date().getTime();
+
+const distance = targetDate - now;
+
+const days =
+Math.floor(distance / (1000 * 60 * 60 * 24));
+
+const hours =
+Math.floor((distance % (1000 * 60 * 60 * 24))
+/ (1000 * 60 * 60));
+
+const minutes =
+Math.floor((distance % (1000 * 60 * 60))
+/ (1000 * 60));
+
+const seconds =
+Math.floor((distance % (1000 * 60))
+/ 1000);
+
+document.getElementById("days").textContent = days;
+document.getElementById("hours").textContent = hours;
+document.getElementById("minutes").textContent = minutes;
+document.getElementById("seconds").textContent = seconds;
+
+}
+
+updateCountdown();
+setInterval(updateCountdown,1000);
