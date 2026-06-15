43635d038f1174c3032826c644007fae32f6485f
assalikiin <fairuznajib04@gmail.com>
2026-06-15 19:35:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -215,3 +215,31 @@ window.addEventListener("scroll", () => {
     Math.max(1 - scroll / 700, 0);
 
 });
+
+const themeToggle =
+document.getElementById("themeToggle");
+
+const savedTheme =
+localStorage.getItem("theme");
+
+if(savedTheme === "light"){
+document.body.classList.add("light");
+themeToggle.textContent = "☀️";
+}
+
+themeToggle.addEventListener("click",()=>{
+
+document.body.classList.toggle("light");
+
+const isLight =
+document.body.classList.contains("light");
+
+themeToggle.textContent =
+isLight ? "☀️" : "🌙";
+
+localStorage.setItem(
+"theme",
+isLight ? "light" : "dark"
+);
+
+});
